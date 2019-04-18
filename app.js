3a37c207f20ce7500d158df956f0c552f1536684
BaeJi77 <hahawjstk@gmail.com>
2019-04-18 17:18:56 +0900

    django to node js

---
+++ app.js
@@ -0,0 +1,11 @@
+var express = require('express'); 
+var app = express(); 
+
+app.get('/', function (req, res) { 
+    console.log(req);
+    res.send('Hello World!'); 
+}) 
+    
+var server = app.listen(3000, function () { 
+    console.log('Server is running!'); 
+})
