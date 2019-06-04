c5dbb549d0733dca230a30753c9f7223c84f2dd7
BaeJi77 <hahawjstk@gmail.com>
2019-06-05 01:21:20 +0900

    implement picture, birth, death date

--- modules/parser.js
+++ modules/parser.js
@@ -1,6 +1,51 @@
+const nowYear = "2019";
+
 module.exports = {
-    startEndYearParsing: function (str) {
-        let parsingArray = str.split('~');
-        console.log(parsingArray);
+    startEndYearParsing: function (date) {
+        var retObject = {};
+        let parsingArray = date.split('~');
+        if (parsingArray[0] === '' || parsingArray[0] === '\t' || parsingArray[0] === '\n') {
+            retObject.start = "-1";
+            retObject.end = "-1";
+        } else if (parsingArray.length === 1) {
+            retObject.start = parsingArray[0];
+            retObject.end = "-1";
+        } else if (parsingArray[1] === '' || parsingArray[1] === '\t' || parsingArray[1] === '\n') {
+            retObject.start = parsingArray[0];
+            retObject.end = nowYear;
+        } else {
+            retObject.start = parsingArray[0];
+            retObject.end = parsingArray[1];
+        }
+
+        return retObject;
+    },
+
+    convertYearMonthToYear: function (yearAndMonth) {
+        return yearAndMonth.split('.')[0];
+    },
+
+    makeBirthDate: function (birthDate) {
+        var onlyYear = birthDate.split('년');
+        var onlyYearNoDetail = onlyYear[0].split(' ');
+        var ret = "";
+        if (onlyYearNoDetail.length === 2) {
+            ret = onlyYearNoDetail[1];
+        } else {
+            ret = onlyYearNoDetail[0];
+        }
+        return ret.trim();
+    },
+
+    makeDeathDate: function (deathDate) {
+        var onlyYear = deathDate.split('년');
+        var onlyYearNoDetail = onlyYear[0].split(' ');
+        var ret = "";
+        if (onlyYearNoDetail.length === 2) {
+            ret = onlyYearNoDetail[1];
+        } else {
+            ret = onlyYearNoDetail[0];
+        }
+        return ret.trim();
     }
 };
